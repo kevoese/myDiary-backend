73d4e57b3f58ff398f5ea156d129de163b7ff834
Kelvin Esegbona <kevoesegbona@gmail.com>
2019-12-04 22:37:25 +0100

    set up graphql environment

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,36 @@
+import * as express from 'express';
+import { ApolloServer,  gql } from 'apollo-server-express';
+import * as cors from 'cors';
 import databaseConnection from './database';
+import { PORT } from './env';
 
+const app = express();
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
-export default { databaseConnection };
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+ 
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.applyMiddleware({ app });
+
+app.listen(PORT || 4000, () => {
+  console.log(`server start at port ${PORT}`);
+});
+
+export default { app, databaseConnection };
